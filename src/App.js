df31d1195f7a02a7689a134aeeb0b5485f72597e
Benita Anawonah <benitaanawonah33@gmail.com>
2022-05-17 22:50:20 +0100

    installed bootstrap

--- src/App.js
+++ src/App.js
@@ -3,11 +3,17 @@ import './App.css';
 
 export default function App() {
   return <div className="App">
+    <div class="container">
    <h1>Weather App</h1> 
-   <footer>This project was coded by Benita Anawonah and is {" "}</footer>
+  
+
+
+   <footer>This project was coded by Benita Anawonah and is {" "}
    <a href='https://github.com/benitaanawonah/react-weather-app' target='_blank'>
      open-sourced on Github
    </a>
+   </footer>
+    </div>
     </div>
   
 }
