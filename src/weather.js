73cc2e11eef07be2977f18776cf1d31c0489ace2
Benita Anawonah <benitaanawonah33@gmail.com>
2022-05-18 19:09:12 +0100

    Added basic styling

--- src/weather.js
+++ src/weather.js
@@ -26,7 +26,8 @@ export default function Weather(props) {
         <form>
             <div className="row">
             <div className="col-9">
-            <input type="search" placeholder="Enter a city..." className="form-control" />
+            <input type="search" placeholder="Enter a city..." className="form-control"
+            autoFocus="on" />
             </div>
             <div className="col-3">
             <input type="submit" value="Search" className="btn btn-primary w-100" />
@@ -47,13 +48,13 @@ export default function Weather(props) {
                 <img
                  src={weatherData.iconUrl} 
                 alt={weatherData.description} 
-                className="float-left" 
+                 className="float-left" 
                 />
-
-                <div className="float-left">
-                    <span className="temperature">{Math.round(weatherData.temperature)}</span>    
-                    <span className="unit">°C</span>
-                </div>
+                
+                <span className="temperature">{Math.round(weatherData.temperature)}</span>    
+                <span className="unit">°C</span>
+                
+                
                 </div>
                 </div>
             <div className="col-6">
