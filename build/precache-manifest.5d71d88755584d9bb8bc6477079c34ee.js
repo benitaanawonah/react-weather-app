1502e2629fc8b85a427c020dd597674a00bd1c11
Benita Anawonah <benitaanawonah33@gmail.com>
2022-07-23 17:58:06 +0100

    build file

---
+++ build/precache-manifest.5d71d88755584d9bb8bc6477079c34ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e2ead2691ebe5ffce824",
+    "url": "/static/css/main.ac6ed8a4.chunk.css"
+  },
+  {
+    "revision": "e2ead2691ebe5ffce824",
+    "url": "/static/js/main.e2ead269.chunk.js"
+  },
+  {
+    "revision": "5ee476ae1244b517c019",
+    "url": "/static/js/1.5ee476ae.chunk.js"
+  },
+  {
+    "revision": "c5b97e8a9c44ead2bc85",
+    "url": "/static/css/2.15a26429.chunk.css"
+  },
+  {
+    "revision": "c5b97e8a9c44ead2bc85",
+    "url": "/static/js/2.c5b97e8a.chunk.js"
+  },
+  {
+    "revision": "d415269a3c0ab26ca93f",
+    "url": "/static/js/runtime~main.d415269a.js"
+  },
+  {
+    "revision": "604e05d79bf25ffed454fef62feabf8a",
+    "url": "/index.html"
+  }
+];
